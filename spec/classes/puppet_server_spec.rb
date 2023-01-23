f5339f1f85e02909860c83dc4a9b1ff4d0400af4
Pat Riehecky <riehecky@fnal.gov>
2023-01-23 15:06:34 -0600

    Fixes #35983 - Permit recursive ownership/permissions for environments dir

--- spec/classes/puppet_server_spec.rb
+++ spec/classes/puppet_server_spec.rb
@@ -125,6 +125,7 @@ describe 'puppet' do
             .with_ensure('directory')
             .with_owner('puppet')
             .with_group(nil)
+            .with_recurse(false)
             .with_mode('0755')
 
           should contain_file(sharedir).with_ensure('directory')
@@ -411,6 +412,11 @@ describe 'puppet' do
       context 'with directory environments owner' do
         let(:params) { super().merge(server_environments_owner: 'apache') }
         it { should contain_file(environments_dir).with_owner('apache') }
+      end
+
+      context 'with directory environments recursive mangement' do
+        let(:params) { super().merge(server_environments_recurse: true) }
+        it { should contain_file(environments_dir).with_recurse(true) }
       end
 
       context 'with no common modules directory' do
